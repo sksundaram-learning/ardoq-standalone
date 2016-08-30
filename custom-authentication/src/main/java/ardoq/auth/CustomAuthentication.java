bd2caf471fe5ba208d47a66e30075a4328c007f3
Kristian Helgesen <kristian@ardoq.com>
2016-08-30 10:33:41 +0200

    Example implementation of custom authentication for Ardoq

--- custom-authentication/src/main/java/ardoq/auth/CustomAuthentication.java
+++ custom-authentication/src/main/java/ardoq/auth/CustomAuthentication.java
@@ -1,20 +1,37 @@
 package ardoq.auth;
-import javax.naming.*;
-import javax.naming.directory.*;
-
 import java.util.Hashtable;
 
-public class CustomAuthentication {
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+
+
+/**
+ * Example implementation of custom authentication for Ardoq.
+ *
+ * Ardoq API looks for a class with
+ * - name ardoq.auth.CustomAuthentication
+ * - empty constructor
+ * - implementing methods from interface ardoq.auth.ICustomAuthentication
+ *
+ * @author Kristian Helgesen, Ardoq AS
+ */
+public class CustomAuthentication implements ICustomAuthentication {
 
     public static void main(String[] args) {
-        authenticate("Kristian Helgesen", "ldap123");
+        new CustomAuthentication().authenticate("Kristian Helgesen", "ldap123");
     }
 
-    public static boolean authenticate(String username, String password) {
+    public ArdoqUser authenticate(String username, String password) {
         return authenticate("ldap://localhost:389", username, password);
     }
 
-    public static boolean authenticate(String providerURL, String username, String password) {
+    public String status() {
+        return "ok";
+    }
+
+    public ArdoqUser authenticate(String providerURL, String username, String password) {
         System.out.println("authenticating: "+username);
 
         // Set up environment for creating initial context
@@ -29,13 +46,17 @@ public class CustomAuthentication {
         try {
             // Create initial context
             DirContext ctx = new InitialDirContext(env);
-            System.out.println("ok "+ctx.getAttributes("cn="+username+",dc=example,dc=org"));
+            System.out.println("ok from LDAP server: "+ctx.getAttributes("cn="+username+",dc=example,dc=org"));
             ctx.close();
 
-            return true;
+            ArdoqUser user = new ArdoqUser();
+            user.setEmail("kristian+custom4@ardoq.com");
+            user.setFullname("Kristian Helgesen");
+            return user;
         } catch (NamingException e) {
             e.printStackTrace();
         }
-        return false;
+        return null;
     }
+
 }
---
+++ custom-authentication/src/main/java/ardoq/auth/ICustomAuthentication.java
@@ -0,0 +1,31 @@
+package ardoq.auth;
+
+/**
+ * Don't change this interface - the Ardoq API server expects it to be excactly like this.
+ *
+ * @author Kristian Helgesen, Ardoq AS
+ */
+public interface ICustomAuthentication {
+    public String status();
+
+    public ArdoqUser authenticate(String providerURL, String username, String password);
+
+    public static class ArdoqUser {
+        private String email;
+        private String fullname;
+
+        public String getEmail() {
+            return email;
+        }
+        public void setEmail(String email) {
+            this.email = email;
+        }
+        public String getFullname() {
+            return fullname;
+        }
+        public void setFullname(String fullname) {
+            this.fullname = fullname;
+        }
+    }
+
+}
